c98a10271d9564157819e688536cac6487d44b96
ikkez <ikkez0n3@gmail.com>
2017-02-17 14:44:09 +0100

    update phpdoc

--- lib/flash.php
+++ lib/flash.php
@@ -11,11 +11,12 @@
  * Christian Knuth <ikkez0n3@gmail.com>
  * https://github.com/ikkez/F3-Sugar/
  *
- * @version 0.2.1
- * @date: 21.01.2015
+ * @version 1.0.0
+ * @date: 17.02.2017
+ * @since: 21.01.2015
  **/
 
-class Flash extends Prefab {
+class Flash extends \Prefab {
 
 	/** @var \Base */
 	protected $f3;
@@ -26,34 +27,64 @@ class Flash extends Prefab {
 	/** @var array */
 	protected $key;
 
+	/**
+	 * Flash constructor.
+	 * @param string $key
+	 */
 	public function __construct($key='flash') {
 		$this->f3 = \Base::instance();
 		$this->msg = &$this->f3->ref('SESSION.'.$key.'.msg');
 		$this->key = &$this->f3->ref('SESSION.'.$key.'.key');
 	}
 
+	/**
+	 * add a message to the stack
+	 * @param $text
+	 * @param string $status
+	 */
 	public function addMessage($text,$status='info') {
 		$this->msg[] = array('text'=>$text,'status'=>$status);
 	}
 
+	/**
+	 * dump all messages and clear them
+	 * @return array
+	 */
 	public function getMessages() {
 		$out = $this->msg;
 		$this->clearMessages();
 		return $out;
 	}
 
+	/**
+	 * reset message stack
+	 */
 	public function clearMessages() {
 		$this->msg = array();
 	}
 
+	/**
+	 * check if there messages in the stack
+	 * @return bool
+	 */
 	public function hasMessages() {
 		return !empty($this->msg);
 	}
 
+	/**
+	 * set a flash key
+	 * @param $key
+	 * @param bool $val
+	 */
 	public function setKey($key,$val=TRUE) {
 		$this->key[$key] = $val;
 	}
 
+	/**
+	 * get and clear a flash key, if it's existing
+	 * @param $key
+	 * @return mixed|null
+	 */
 	public function getKey($key) {
 		$out = NULL;
 		if ($this->hasKey($key)) {
@@ -63,6 +94,11 @@ class Flash extends Prefab {
 		return $out;
 	}
 
+	/**
+	 * check if there's a flash key existing
+	 * @param $key
+	 * @return bool
+	 */
 	public function hasKey($key) {
 		return ($this->key && array_key_exists($key, $this->key));
 	}
